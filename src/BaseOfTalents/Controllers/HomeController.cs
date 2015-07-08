a2b9c4c840fb6a4ea6f11395728c6387b188b61b
Igor <vskpsn@mail.ru>
2015-07-08 17:50:50 +0300

    View added

---
+++ src/BaseOfTalents/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaseOfTalents.DAL.Repositories;
+
+namespace BaseOfTalents.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Candidate = null;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Name)
+        {
+            ViewBag.Candidate = new MainRepository().GetCandidateByName(Name);
+            return View();
+        }
+    }
+}
